cf7727a934fe66519d6217bcce7793d7016aca3b
charupraneeth <charupraneeth@gmail.com>
2021-02-11 10:33:46 +0530

    fixed the loading more bug and added loader

--- src/views/Subreddit.vue
+++ src/views/Subreddit.vue
@@ -19,10 +19,18 @@
       class="blue-text"
       @click="loadMorePosts"
       style="cursor:pointer"
-      v-if="postsState.after && !postsState.loading && !postsState.error"
+      v-if="
+        postsState.after && !postsState.error && postsLength != posts.length
+      "
     >
       load more...
     </p>
+  </div>
+  <div
+    class="progress blue lighten-3"
+    v-show="postsState.loading && postsLength"
+  >
+    <div class="indeterminate orange"></div>
   </div>
 </template>
 
@@ -31,7 +39,7 @@ import API from "@/lib/API";
 import SubredditName from "@/store/state";
 import RedditPost from "@/components/RedditPost.vue";
 import SubredditHeader from "@/components/SubredditHeader.vue";
-import { computed, watch } from "vue";
+import { computed, watch, ref } from "vue";
 import usePosts from "@/hooks/usePosts.js";
 export default {
   name: "Subreddit",
@@ -51,23 +59,46 @@ export default {
       { immediate: true }
     );
     // const headerData = useHeader(SubredditName)
+
     const postsState = usePosts(SubredditName);
     const posts = computed(() => postsState.data.map((child) => child.data));
+    const postsLength = ref(0);
     async function loadMorePosts() {
-      // console.log(postsState);
+      postsState.loading = true;
+      console.log(postsLength.value);
+      postsLength.value = posts.value.length;
       const morePosts = await API.getPosts(
         SubredditName.value,
         postsState.after
       );
       postsState.data.push(...morePosts.data.children);
       postsState.after = morePosts.data.after;
+      console.log(postsLength.value);
+      postsState.loading = false;
       // console.log(morePosts.data.children);
     }
-
+    // function bottomVisible() {
+    //   const scrollY = window.scrollY;
+    //   const visible = document.documentElement.clientHeight;
+    //   const pageHeight = document.documentElement.scrollHeight + 100;
+    //   const bottomOfPage = visible + scrollY + 100 >= pageHeight;
+    //   // console.log(
+    //   //   `visible : ${visible}, scrollY : ${scrollY},pageHeight:${pageHeight}`
+    //   // );
+    //   return bottomOfPage || pageHeight < visible;
+    // }
+    // onMounted(() => {
+    //   window.addEventListener("scroll", async () => {
+    //     bottom.value = bottomVisible();
+    //     console.log(bottom.value);
+    //     if (bottom.value) loadMorePosts();
+    //   });
+    // });
     return {
       postsState,
       posts,
       loadMorePosts,
+      postsLength,
     };
   },
 };
