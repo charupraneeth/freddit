1275ca2fdefbc31742cf81d80e753e135f19c437
charupraneeth <charupraneeth@gmail.com>
2021-06-30 12:44:45 +0530

    :ambulance: !BREAKING: add pwa support

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "materialize-css/dist/css/materialize.min.css";
 import router from './router'
+import './registerServiceWorker'
 
 createApp(App).use(router).mount("#app");
